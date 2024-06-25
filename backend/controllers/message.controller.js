99c263d07c72af16cf58b018cf36567ff96c303b
Vijay <vpandit142@gmail.com>
2024-06-26 01:58:44 +0530

    backend:get messages for a particular user

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -3,6 +3,22 @@ import bcrypt from "bcryptjs";
 import gennerateTokenAndSetCookie from "../utils/generateToken.js";
 import Conversation from "../models/conversation.model.js";
 
+export const getMessages = async (req, res) => {
+  try {
+    const senderId = req.user?._id;
+    const { id: receiverId } = req.params;
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+    const messages = await Message.find({
+      _id: { $in: conversation?.messages || [] },
+    });
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller ", error.message);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
@@ -26,10 +42,12 @@ export const sendMessage = async (req, res) => {
     });
 
     if (newMessage) {
-      conversation?.messages?.push(newMessage._id);
-      await conversation.save();
+      conversation.messages
+        ? conversation.messages.push(newMessage._id)
+        : (conversation.messages = [newMessage._id]);
     }
 
+    await conversation.save();
     await newMessage.save();
 
     res.status(200).json(newMessage);
--- backend/models/conversation.model.js
+++ backend/models/conversation.model.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const conversation = new mongoose.Schema(
   {
     participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  },
-  {
+
     messages: [
       { type: mongoose.Schema.Types.ObjectId, ref: "Message", default: [] },
     ],
